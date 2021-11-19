5a1be1d9ad423b39ea6a04739e9050f9974b3aa5
marshall7m <44485531+marshall7m@users.noreply.github.com>
2021-11-18 23:28:52 -0800

    fixture syntax tmpdir

--- buildspecs/tests/fixtures/mock_repo.py
+++ buildspecs/tests/fixtures/mock_repo.py
@@ -9,13 +9,6 @@ import shutil
 log = logging.getLogger(__name__)
 log.setLevel(logging.DEBUG)
 
-
-# TODO: create class for fixtures to allow for access to global vars such as remote_url, session dir, function dirv via class instance?
-# Example: repo_one = MockRepo(<git_url>) 
-# repo_one.url
-# repo_one.session_dir
-# repo_one.function_dir
-
 @pytest.fixture(scope="session", autouse=True)
 def session_repo_dir(tmpdir_factory):
     dir = tmpdir_factory.mktemp('test-repo')
@@ -56,9 +49,8 @@ def apply_session_mock_repo(session_repo_dir):
     subprocess.run(cmd, capture_output=False, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
 @pytest.fixture(scope="function", autouse=True)
-def create_test_function_tf_state(tmp_dir):
-    test_case_tf_state_parent_dir = tmp_dir.mkdir('tf-state')
-    
+def create_test_function_tf_state(tmpdir):
+    test_case_tf_state_parent_dir = tmpdir.mkdir('tf-state')
     os.chdir(os.environ['TESTING_LOCAL_PARENT_TF_STATE_DIR'])
     for path in Path('.').rglob('*.tfstate'):
         new_path = os.path.join('../doo', path)
@@ -67,7 +59,7 @@ def create_test_function_tf_state(tmp_dir):
 
     # changing TESTING_LOCAL_PARENT_TF_STATE_DIR to test case tf-state dir to create persistant local tf-state
     # prevents loss of local tf-state when new github branches are created/checked out for mocking commits
-    os.environ['TESTING_LOCAL_PARENT_TF_STATE_DIR'] = test_case_tf_state_parent_dir
+    os.environ['TESTING_LOCAL_PARENT_TF_STATE_DIR'] = str(test_case_tf_state_parent_dir)
 
 def setup_terragrunt_branch_tracking():
     pass
