e566c929bc7e0afff648d1dddfbae72f24370553
marshall7m <44485531+marshall7m@users.noreply.github.com>
2021-11-29 13:38:24 -0800

    mv scenarios to separate file in fixt dir

---
+++ buildspecs/tests/fixtures/scenarios.py
@@ -0,0 +1,204 @@
+import pytest
+import logging
+import os
+import psycopg2
+import json
+from helpers.utils import TestSetup
+
+log = logging.getLogger(__name__)
+log.setLevel(logging.DEBUG)
+
+
+@pytest.fixture()
+def scenario_1(test_id, repo_url, function_repo_dir):
+    '''
+    CW Event: 
+        status: success
+        is_rollback: false
+        is_base_rollback: false
+        cfg_path: 1/2
+    
+    SF Execution:
+        PR: Same as CW event
+        commit: Same as CW event
+        is_rollback: false
+        is_base_rollback: false
+        cfg_path: 2/2
+    '''
+    log.debug('Running scenario')
+
+    with TestSetup(psycopg2.connect(), repo_url, function_repo_dir, os.environ['GITHUB_TOKEN']) as ts:
+        pr = ts.pr(base_ref=os.environ['BASE_REF'], head_ref=f'feature-{test_id}')
+
+        pr.create_pr(status='running')
+
+        pr.create_commit(
+            status='running',
+            modify_items=[
+                {
+                    'apply_changes': True,
+                    'cfg_path': 'directory_dependency/dev-account/us-west-2/env-one/doo',
+                    'create_provider_resource': False,
+                    'execution': {
+                        'is_base_rollback': False,
+                        'is_rollback': False,
+                        'account_name': 'dev',
+                        'cw_event_finished_status': 'success'
+                    }
+                },
+                {
+                    'apply_changes': False,
+                    'cfg_path': 'directory_dependency/dev-account/us-west-2/env-one/bar',
+                    'create_provider_resource': False,
+                    'execution': {
+                        'status': 'waiting',
+                        'is_base_rollback': False,
+                        'is_rollback': False,
+                        'account_name': 'dev'
+                    }
+                }
+            ]
+        )
+
+        pr.insert_records()
+        cw_event = json.loads(os.environ['EVENTBRIDGE_EVENT'])
+        pr.collect_record_assertion('executions', {**cw_event, **{'status': 'success'}}, [cw_event])
+        pr.collect_record_assertion('commit_queue', {**pr.commit_records[0], **{'status': 'running'}}, [pr.commit_records[0]])
+        pr.collect_record_assertion('pr_queue', {**pr.pr_record, **{'status': 'running'}}, [{'pr_id': pr.pr_record['pr_id']}])
+        
+    return ts
+
+@pytest.fixture()
+def scenario_2(test_id, repo_url, function_repo_dir):
+    '''
+    CW Event: 
+        status: success
+        is_rollback: false
+        is_base_rollback: false
+        cfg_path: 1/1
+    
+    SF Execution:
+        PR: Same as CW event
+        commit: New commit
+        is_rollback: false
+        is_base_rollback: false
+        cfg_path: 1/2
+    '''
+    log.debug('Running scenario')
+    
+    with TestSetup(psycopg2.connect(), repo_url, function_repo_dir, os.environ['GITHUB_TOKEN']) as ts:
+        pr = ts.pr(base_ref=os.environ['BASE_REF'], head_ref=f'feature-{test_id}')
+
+        pr.create_pr(status='running')
+
+        pr.create_commit(
+            status='running',
+            modify_items=[
+                {
+                    'apply_changes': True,
+                    'cfg_path': 'directory_dependency/dev-account/us-west-2/env-one/doo',
+                    'create_provider_resource': False,
+                    'execution': {
+                        'is_base_rollback': False,
+                        'is_rollback': False,
+                        'account_name': 'dev',
+                        'cw_event_finished_status': 'success'
+                    }
+                }
+            ]
+        )
+
+        next_commit_path = 'directory_dependency/dev-account/us-west-2/env-one/bar'
+        pr.create_commit(
+            status='waiting',
+            modify_items=[
+                {
+                    'apply_changes': False,
+                    'cfg_path': next_commit_path,
+                    'create_provider_resource': False,
+                }
+            ]
+        )
+
+        pr.insert_records()
+
+        cw_event = json.loads(os.environ['EVENTBRIDGE_EVENT'])
+        pr.collect_record_assertion('executions', {**cw_event, **{'status': 'success'}}, [cw_event])
+        pr.collect_record_assertion('commit_queue', {**pr.commit_records[0], **{'status': 'success'}}, [pr.commit_records[0]])
+        pr.collect_record_assertion('commit_queue', {**pr.commit_records[1], **{'status': 'running'}}, [pr.commit_records[1]])
+        pr.collect_record_assertion('executions', {'status': 'running', 'cfg_path': next_commit_path}, [pr.execution_records[0]])
+        pr.collect_record_assertion('pr_queue', {**pr.pr_record, **{'status': 'running'}}, [pr.pr_record])
+        
+    return ts
+
+@pytest.fixture()
+def scenario_3(test_id, repo_url, function_repo_dir):
+    '''
+    CW Event: 
+        status: failed
+        is_rollback: false
+        is_base_rollback: false
+        cfg_path: 1/1
+    
+    SF Execution:
+        PR: Same as CW event
+        commit: Same as CW event
+        is_rollback: true
+        is_base_rollback: true
+        cfg_path: 1/1
+    '''
+    log.debug('Running scenario')
+    
+    with TestSetup(psycopg2.connect(), repo_url, function_repo_dir, os.environ['GITHUB_TOKEN']) as ts:
+        pr = ts.pr(base_ref=os.environ['BASE_REF'], head_ref=f'feature-{test_id}')
+
+        pr.create_pr(status='running')
+        cw_cfg_path = 'directory_dependency/dev-account/us-west-2/env-one/doo'
+        pr.create_commit(
+            status='running',
+            modify_items=[
+                {
+                    'apply_changes': True,
+                    'cfg_path': cw_cfg_path,
+                    'create_provider_resource': False,
+                    'execution': {
+                        'is_base_rollback': False,
+                        'is_rollback': False,
+                        'account_name': 'dev',
+                        'cw_event_finished_status': 'failed'
+                    }
+                }
+            ]
+        )
+
+        pr.create_commit(
+            status='waiting',
+            modify_items=[
+                {
+                    'apply_changes': False,
+                    'cfg_path': 'directory_dependency/dev-account/us-west-2/env-one/bar',
+                    'create_provider_resource': False,
+                }
+            ]
+        )
+
+        pr.insert_records()
+
+        cw_event = json.loads(os.environ['EVENTBRIDGE_EVENT'])
+        pr.collect_record_assertion('executions', {**cw_event, **{'status': 'failed'}}, [cw_event])
+        
+        pr.collect_record_assertion('commit_queue', {**pr.commit_records[0], **{'status': 'failed'}}, [pr.commit_records[0]])
+        pr.collect_record_assertion('commit_queue', {**pr.commit_records[0], **{'status': 'running', 'is_rollback': True}}, [pr.commit_records[0]])
+        pr.collect_record_assertion('commit_queue', {**pr.commit_records[1], **{'status': 'waiting'}}, [pr.commit_records[1]])
+
+        pr.collect_record_assertion('executions', {
+            'status': 'running',
+            'cfg_path': cw_cfg_path,
+            'commit_id': pr.get_base_commit_id(),
+            'is_rollback': True,
+            'is_base_rollback': True
+        }, [pr.execution_records[0]])
+
+        pr.collect_record_assertion('pr_queue', {**pr.pr_record, **{'status': 'running'}}, [pr.pr_record])
+        
+    return ts
--- buildspecs/tests/test_trigger_sf.py
+++ buildspecs/tests/test_trigger_sf.py
@@ -5,6 +5,7 @@ import logging
 from buildspecs.trigger_sf import TriggerSF
 from psycopg2.sql import SQL
 from helpers.utils import TestSetup
+
 import uuid
 import json
 
@@ -20,65 +21,6 @@ def codebuild_env():
 
     os.environ['DRY_RUN'] = 'true'
     os.environ['PGOPTIONS'] = '-c statement_timeout=100000'
-
-@pytest.fixture()
-def scenario_1(test_id, repo_url, function_repo_dir):
-    '''
-    CW Event: 
-        status: success
-        is_rollback: false
-        is_base_rollback: false
-        cfg_path: 1/2
-    
-    SF Execution:
-        PR: Same as CW event
-        commit: Same as CW event
-        is_rollback: false
-        is_base_rollback: false
-        cfg_path: 2/2
-    '''
-    log.debug('Running scenario')
-    conn = psycopg2.connect()
-    with TestSetup(conn, repo_url, function_repo_dir, os.environ['GITHUB_TOKEN']) as ts:
-        pr = ts.pr(base_ref=os.environ['BASE_REF'], head_ref=f'feature-{test_id}')
-
-        pr.create_pr(status='running')
-
-        pr.create_commit(
-            status='waiting',
-            modify_items=[
-                {
-                    'apply_changes': True,
-                    'cfg_path': 'directory_dependency/dev-account/us-west-2/env-one/doo',
-                    'create_provider_resource': False,
-                    'execution': {
-                        'is_base_rollback': False,
-                        'is_rollback': False,
-                        'account_name': 'dev',
-                        'cw_event_finished_status': 'success'
-                    }
-                },
-                {
-                    'apply_changes': False,
-                    'cfg_path': 'directory_dependency/dev-account/us-west-2/env-one/bar',
-                    'create_provider_resource': False,
-                    'execution': {
-                        'status': 'waiting',
-                        'is_base_rollback': False,
-                        'is_rollback': False,
-                        'account_name': 'dev'
-                    }
-                }
-            ]
-        )
-
-        pr.insert_records()
-
-        pr.collect_record_assertion('executions', {**json.loads(os.environ['EVENTBRIDGE_EVENT']), **{'status': 'success'}})
-        pr.collect_record_assertion('commit_queue', {**pr.commit_records[0], **{'status': 'running'}})
-        pr.collect_record_assertion('pr_queue', {**pr.pr_record, **{'status': 'running'}})
-        
-    return ts
 
 @pytest.mark.parametrize("scenario", [
     ("scenario_1")
@@ -99,5 +41,6 @@ def test_record_exists(scenario, request):
         trigger.cleanup()
 
     log.info("Running record assertions:")
-    with psycopg2.connect() as conn:
+    with psycopg2.connect() as conn:        
         scenario.run_collected_assertions(conn)
+
