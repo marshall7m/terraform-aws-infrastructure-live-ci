3d6b2443d8cdca4575f3c04cf32fd92eddfd5bc5
marshall7m <44485531+marshall7m@users.noreply.github.com>
2021-12-18 23:27:04 -0800

    reverse rollback cfg_deps to simplify get target ids

--- buildspecs/sql/select_target_execution_ids.sql
+++ buildspecs/sql/select_target_execution_ids.sql
@@ -16,84 +16,50 @@ CREATE OR REPLACE FUNCTION arr_in_arr_count(TEXT[], TEXT[]) RETURNS INT AS $$
 $$ LANGUAGE plpgsql;
 
 CREATE OR REPLACE FUNCTION get_target_execution_ids() RETURNS TEXT[] AS $$
-    DECLARE
-        _is_rollback BOOLEAN;
     BEGIN
-        -- gets all executions from running commit
         CREATE TABLE commit_executions AS
-            SELECT *
+        WITH target_commit AS (
+            SELECT DISTINCT commit_id, is_rollback 
             FROM executions
-            WHERE commit_id = (
-                SELECT DISTINCT(commit_id)
-                FROM executions
-                WHERE "status" = 'waiting'
-            );
+            WHERE "status" = 'waiting'
+        )
+        SELECT *
+        FROM executions
+        -- intentionally errors if there are more than one unique commit_id and/or is_rollback value
+        WHERE commit_id = (SELECT commit_id FROM target_commit)
+        AND is_rollback = (SELECT is_rollback FROM target_commit);
 
-        SELECT DISTINCT(is_rollback)
-        INTO _is_rollback
-        FROM commit_executions;
-
-        -- get all executions that are waiting within commit
         CREATE TABLE queued_executions AS
-            SELECT *
-            FROM commit_executions
-            WHERE "status" = 'waiting';
+        SELECT *
+        FROM commit_executions
+        WHERE "status" = 'waiting';
 
-        IF _is_rollback = true THEN
-            RAISE NOTICE 'Getting target rollback executions';
-            -- selects executions where all account/terragrunt config dependencies are successful
-            -- TODO: add doc
-            RETURN (
-                SELECT array_agg(execution_id::TEXT)
-                FROM queued_executions
-                WHERE account_path NOT IN (
-                    SELECT c.account_deps
-                    FROM   commit_executions t
-                    LEFT JOIN unnest(t.account_deps) c(account_deps) 
-                    ON true
-                    WHERE "status" = 'running'
-                    AND t.account_deps IS NOT NULL
-                    AND cardinality(t.account_deps) > 0
-                )
-                AND cfg_path NOT IN (
-                    SELECT c.cfg_deps
-                    FROM   executions t
-                    LEFT JOIN unnest(t.cfg_deps) c(cfg_deps)
-                    ON true
-                    WHERE "status" = 'running'
-                    AND t.cfg_deps IS NOT NULL
-                    AND cardinality(t.cfg_deps) > 0
-                )
-            );
-            -- none of the cfg_deps are running/
-        ELSE
-            RAISE NOTICE 'Getting target deployment executions';
-            -- selects executions where all account/terragrunt config dependencies are successful
-            RETURN (
-                SELECT array_agg(execution_id::TEXT)
-                FROM queued_executions
-                -- where count of dependency array == the count of successful dependencies
-                WHERE cardinality(account_deps) = arr_in_arr_count(
-                    account_deps, ( 
-                        SELECT ARRAY(
-                            SELECT DISTINCT account_name
-                            FROM commit_executions
-                            GROUP BY account_name
-                            HAVING COUNT(*) FILTER (WHERE "status" = 'success') = COUNT(*)
-                        )
+        RAISE NOTICE 'Getting target executions';
+        -- selects executions where all account/terragrunt config dependencies are successful
+        RETURN (
+            SELECT array_agg(execution_id::TEXT)
+            FROM queued_executions
+            -- where count of dependency array == the count of successful dependencies
+            WHERE cardinality(account_deps) = arr_in_arr_count(
+                account_deps, ( 
+                    SELECT ARRAY(
+                        SELECT DISTINCT account_name
+                        FROM commit_executions
+                        GROUP BY account_name
+                        HAVING COUNT(*) FILTER (WHERE "status" = 'success') = COUNT(*)
                     )
                 )
-                AND cardinality(cfg_deps) = arr_in_arr_count(
-                    cfg_deps, (
-                        SELECT ARRAY(
-                            SELECT DISTINCT commit_executions.cfg_path
-                            FROM commit_executions
-                            WHERE "status" = 'success'
-                        )
+            )
+            AND cardinality(cfg_deps) = arr_in_arr_count(
+                cfg_deps, (
+                    SELECT ARRAY(
+                        SELECT DISTINCT commit_executions.cfg_path
+                        FROM commit_executions
+                        WHERE "status" = 'success'
                     )
                 )
-            );
-        END IF;
+            )
+        );
     END;
 $$ LANGUAGE plpgsql;
 
--- buildspecs/sql/update_executions_with_new_rollback_stack.sql
+++ buildspecs/sql/update_executions_with_new_rollback_stack.sql
@@ -19,7 +19,6 @@ INSERT INTO executions (
     commit_id,
     base_ref,
     head_ref,
-    base_source_version,
     head_source_version,
     cfg_path,
     cfg_deps,
@@ -37,34 +36,65 @@ INSERT INTO executions (
     rejection_count,
     min_rejection_count
 )
-SELECT
-    'run-' || substr(md5(random()::text), 0, 8) as execution_id,
-    true as is_rollback,
-    false as is_base_rollback,
+SELECT 
+    execution_id,
+    is_rollback,
+    is_base_rollback,
     pr_id,
     commit_id,
     base_ref,
     head_ref,
-    base_source_version,
     head_source_version,
     cfg_path,
-    cfg_deps,
-    'waiting' as status,
-    'terragrunt destroy' || target_resources(new_resources) as plan_command,
-    'terragrunt destroy' || target_resources(new_resources) || ' -auto-approve' as deploy_commmand,
+    -- gets cfg dependencies that depend on cfg_path (essentially reversing the dependency tree)
+    ARRAY(
+        SELECT cfg_path
+        FROM executions
+        WHERE d.cfg_path=ANY(cfg_deps)
+        AND commit_id = {commit_id}
+        AND cardinality(new_resources) > 0
+    ) AS cfg_deps,
+    "status",
+    plan_command,
+    deploy_command,
     new_providers,
     new_resources,
     account_name,
     account_path,
     account_deps,
     voters,
-    0 as approval_count,
+    approval_count,
     min_approval_count,
-    0 as rejection_count,
+    rejection_count,
     min_rejection_count
-FROM
-    executions
-WHERE
-    commit_id = {} AND
-    cardinality(new_resources) > 0
+FROM (
+    SELECT
+        'run-' || substr(md5(random()::text), 0, 8) as execution_id,
+        true as is_rollback,
+        false as is_base_rollback,
+        pr_id,
+        commit_id,
+        base_ref,
+        head_ref,
+        head_source_version,
+        cfg_deps,
+        'waiting' as status,
+        'terragrunt destroy' || target_resources(new_resources) as plan_command,
+        'terragrunt destroy' || target_resources(new_resources) || ' -auto-approve' as deploy_commmand,
+        new_providers,
+        new_resources,
+        account_name,
+        account_path,
+        account_deps,
+        voters,
+        0 as approval_count,
+        min_approval_count,
+        0 as rejection_count,
+        min_rejection_count
+    FROM
+        executions
+    WHERE
+        commit_id = {commit_id} 
+        AND cardinality(new_resources) > 0
+) d
 RETURNING *;
