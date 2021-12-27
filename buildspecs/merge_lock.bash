b6bf918cf67a1645cc36a2b1cf2bc60ff97e0667
marshall7m <44485531+marshall7m@users.noreply.github.com>
2021-12-26 19:13:06 -0800

    add merge lock script/tests

---
+++ buildspecs/merge_lock.bash
@@ -0,0 +1,21 @@
+if [ "$MERGE_LOCK" == "true" ]; then
+    running_pr=$(psql -qtAX -c "SELECT DISTINCT pr_id FROM executions WHERE status = 'running'")
+    data=$(cat <<EOF
+    {
+        "state": "pending", 
+        "description": "Merging Terragrunt changes is locked. PR #${running_pr} integration is running"
+    }
+EOF
+    )
+    curl -s -X POST \
+        -H 'Content-Type: application/json' \
+        --data "$data" \
+        https://${GITHUB_TOKEN}:x-oauth-basic@api.github.com/repos/${REPO_FULL_NAME}/statuses/${CODEBUILD_SOURCE_VERSION}
+elif [ "$MERGE_LOCK" == "false" ]; then
+    curl -s -X POST \
+        -H 'Content-Type: application/json' \
+        --data '{"state": "success", "description": "Merging Terragrunt changes is unlocked"}' \
+        https://${GITHUB_TOKEN}:x-oauth-basic@api.github.com/repos/${REPO_FULL_NAME}/statuses/${CODEBUILD_SOURCE_VERSION}
+else
+    echo "Invalid merge lock value: $MERGE_LOCK" && exit 1
+fi
