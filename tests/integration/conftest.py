0f74030f2c839376de2997b31b63bb467bfe6f2b
marshall7m <44485531+marshall7m@users.noreply.github.com>
2022-02-15 10:03:15 -0800

    separate tfenv logic from tf init

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -7,7 +7,9 @@ import github
 import subprocess
 import logging
 import sys
+import shutil
 import aurora_data_api
+import git
 
 log = logging.getLogger(__name__)
 log.setLevel(logging.DEBUG)
@@ -17,8 +19,22 @@ def pytest_addoption(parser):
     parser.addoption("--skip-init", action="store_true", help="skips initing tf module")
     parser.addoption("--skip-apply", action="store_true", help="skips applying tf module")
 
+@pytest.fixture(scope="session", autouse=True)
+def tf_version():
+    tf_dir = os.path.dirname(os.path.realpath(__file__))
+    version = subprocess.run('terraform --version', shell=True, capture_output=True, text=True)
+    if version.returncode == 0:
+        log.info('Terraform found in $PATH -- skip scanning for tf version')
+        log.info(f'Terraform Version: {version.stdout}')
+    else:
+        log.info('Scanning tf config for minimum tf version')
+        out = subprocess.run('tfenv install min-required && tfenv use min-required', 
+            cwd=tf_dir, shell=True, capture_output=True, check=True, text=True
+        )
+        log.debug(f'tfenv out: {out.stdout}')
+
 @pytest.fixture(scope="session")
-def mut(request):
+def mut(request, tf_version):
     tf_dir = os.path.dirname(os.path.realpath(__file__))
 
     if 'GITHUB_TOKEN' not in os.environ:
@@ -29,20 +45,13 @@ def mut(request):
     tf = tftest.TerraformTest(tf_dir)
 
     if getattr(request, 'param', False):
-        log.info('Skip scanning for tf version')
         log.info('Skip initing testing tf module')
     else:
-        log.info('Scanning tf config for minimum tf version')
-        out = subprocess.run('tfenv install min-required && tfenv use min-required', 
-            cwd=tf_dir, shell=True, capture_output=True, check=True, text=True
-        )
-
-        log.debug(f'tfenv out: {out.stdout}')
         log.info('Initing testing tf module')
         tf.init()
 
     yield tf
-
+    
 @pytest.fixture(scope="session")
 def mut_plan(mut, request):
     log.info('Getting testing tf plan')
@@ -62,7 +71,7 @@ def mut_output(mut, request):
 def conn(mut_output):
     conn = aurora_data_api.connect(
         aurora_cluster_arn=mut_output['metadb_arn'],
-        secret_arn=mut_output['metadb_secret_manager_arn'],
+        secret_arn=mut_output['metadb_secret_manager_master_arn'],
         database=mut_output['metadb_name']
     )
 
@@ -96,6 +105,21 @@ def repo(gh, mut_output):
     return repo
 
 @pytest.fixture(scope='class')
-def merge_pr(repo, pr, merge_lock_status):
+def merge_pr(repo, pr, merge_lock_status, tmp_path_factory):
     log.info('Merging PR')
-    yield repo.merge(os.environ['BASE_REF'], os.environ['HEAD_REF'])
+    base_commit_id = repo.get_branch(os.environ['BASE_REF']).commit.sha
+    merge_commit = repo.merge(os.environ['BASE_REF'], os.environ['HEAD_REF'])
+    yield merge_commit
+
+    log.info('Removing PR changes')
+
+    dir = str(tmp_path_factory.mktemp('scenario-repo-'))
+    log.debug(f'Scenario repo dir: {dir}')
+
+    git_repo = git.Repo.clone_from(f'https://oauth2:{os.environ["GITHUB_TOKEN"]}@github.com/{os.environ["REPO_FULL_NAME"]}.git', dir)
+
+    log.info(f'Reverting to commit ID: {base_commit_id}')
+    git_repo.git.revert('-m', '1', merge_commit.sha, no_edit=True)
+    git_repo.git.push('origin')
+
+    shutil.rmtree(dir)
