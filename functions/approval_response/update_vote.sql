07ec1919b4dca3631fff255d779640350846f172
marshall7m <44485531+marshall7m@users.noreply.github.com>
2022-02-22 10:06:32 -0800

    fix approval response psycopg2 conn and update_vote syntax

--- functions/approval_response/update_vote.sql
+++ functions/approval_response/update_vote.sql
@@ -1,21 +1,21 @@
 UPDATE executions
 SET
     approval_voters = CASE 
-        WHEN {action} == 'approve' THEN
+        WHEN {action} = 'approve' THEN
             (
                 SELECT array_agg(DISTINCT e)
-                FROM unnest(approval_voters || ARRAY[{voter}]) e
+                FROM unnest(approval_voters || ARRAY[{recipient}]) e
             )
-        WHEN "action" == 'reject' THEN 
+        WHEN {action} = 'reject' THEN 
             array_remove(approval_voters, {recipient})
     END,
     rejection_voters = CASE 
-        WHEN {action} == 'approve' THEN
+        WHEN {action} = 'approve' THEN
             array_remove(rejection_voters, {recipient})
-        WHEN "action" == 'reject' THEN 
+        WHEN {action} = 'reject' THEN 
             (
                 SELECT array_agg(DISTINCT e)
-                FROM unnest(rejection_voters || ARRAY[{voter}]) e
+                FROM unnest(rejection_voters || ARRAY[{recipient}]) e
             )
     END
 WHERE execution_id = {execution_id}
