b6cf61b6f1606a9a5b196a444719b940796d07fc
marshall7m <44485531+marshall7m@users.noreply.github.com>
2022-03-09 09:27:57 -0800

    add shared tf_version fixt to tests/

---
+++ tests/conftest.py
@@ -0,0 +1,19 @@
+import pytest
+import os
+import subprocess
+import logging
+
+log = logging.getLogger(__name__)
+log.setLevel(logging.DEBUG)
+
+@pytest.fixture(scope="session")
+def tf_version():
+    version = subprocess.run('terraform --version', shell=True, capture_output=True, text=True)
+    if version.returncode == 0:
+        log.info('Terraform found in $PATH -- skip scanning for tf version')
+        log.info(f'Terraform Version: {version.stdout}')
+    else:
+        log.info('Scanning tf config for minimum tf version')
+        out = subprocess.run('tfenv install min-required && tfenv use min-required', shell=True, capture_output=True, check=True, text=True
+        )
+        log.debug(f'tfenv out: {out.stdout}')
