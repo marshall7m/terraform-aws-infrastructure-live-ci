b932133363feae3768c57f59036ae4b6ee039bbf
marshall7m <44485531+marshall7m@users.noreply.github.com>
2022-03-09 09:26:02 -0800

    mv pytest generate tests func to conftest & add individual execution actions to scenarios

--- tests/integration/test_scenarios.py
+++ tests/integration/test_scenarios.py
@@ -13,63 +13,55 @@ output "{random}" {{
 }}
 """
 
-def pytest_generate_tests(metafunc):
-    if 'modify_items' in metafunc.fixturenames:
-        metafunc.parametrize('modify_items', metafunc.cls.scenario['modify_items'], indirect=True)
-    if 'target_execution' in metafunc.fixturenames:
-        metafunc.parametrize('target_execution', list(range(0, len(metafunc.cls.scenario['executions']) + len(metafunc.cls.scenario['rollback_executions']))), indirect=True)
-    if 'scenario' in metafunc.fixturenames:
-        metafunc.parametrize('scenario', [metafunc.cls.scenario], scope='class')
-
-    if metafunc.config.getoption('skip_init'):
-        metafunc.parametrize('mut', [True], scope='class', indirect=True)
-    if metafunc.config.getoption('skip_apply'):
-        metafunc.parametrize('mut_output', [True], scope='class', indirect=True)
-
-class TestScenarioOne(test_integration.TestIntegration):
-    scenario = {
-        'modify_items': [
-            {
-                'cfg_path': 'directory_dependency/dev-account/us-west-2/env-one/doo',
-                'content': test_null_resource
-            }
-        ],
-        'executions': {
-            'directory_dependency/dev-account/us-west-2/env-one/doo': {
-                'action': 'approve'
-            }
-        }
-    }
+# class TestScenarioOne(test_integration.TestIntegration):
+#     scenario = {
+#         'modify_items': [
+#             {
+#                 'cfg_path': 'directory_dependency/dev-account/us-west-2/env-one/doo',
+#                 'content': test_null_resource
+#             }
+#         ],
+#         'executions': {
+#             'directory_dependency/dev-account/us-west-2/env-one/doo': {
+#                 'action': 'approve'
+#             }
+#         }
+#     }
 
 class TestScenarioTwo(test_integration.TestIntegration):
     scenario = {
-        'modify_items': [
-            {
-                'cfg_path': 'directory_dependency/dev-account/global',
-                'content': test_null_resource
-            }
-        ],
-        'executions': {
-            'directory_dependency/dev-account/global': {
-                'action': 'approve'
+        'directory_dependency/dev-account/global': {
+            'actions': {
+                'deploy': 'approve',
+                'rollback_providers': 'approve',
+                'rollback_base': 'approve'
             },
-            'directory_dependency/dev-account/us-west-2/env-one/baz': {
-                'action': 'approve'
-            },
-            'directory_dependency/dev-account/us-west-2/env-one/bar': {
-                'action': 'reject'
-            },
-            'directory_dependency/dev-account/us-west-2/env-one/doo': {
-                'action': 'approve'
-            },
-            'directory_dependency/dev-account/us-west-2/env-one/foo': {
-                'action': 'approve'
+            'new_resources': [],
+            'new_file_content': test_null_resource,
+            'new_providers': ['hashicorp/null']
+        },
+        'directory_dependency/dev-account/us-west-2/env-one/baz': {
+            'actions': {
+                'deploy': 'approve',
+                'rollback_base': 'approve'
             }
         },
-        'rollback_executions': {
-            'directory_dependency/dev-account/global': {
-                'action': 'approve',
-                'new_resources': []
+        'directory_dependency/dev-account/us-west-2/env-one/bar': {
+            'actions': {
+                'deploy': 'reject',
+                'rollback_base': 'approve'
+            }
+        },
+        'directory_dependency/dev-account/us-west-2/env-one/doo': {
+            'actions': {
+                'deploy': 'approve',
+                'rollback_base': 'approve'
+            }
+        },
+        'directory_dependency/dev-account/us-west-2/env-one/foo': {
+            'actions': {
+                'deploy': 'approve',
+                'rollback_base': 'approve'
             }
         }
     }
