fa216f273cbd3ef71fc2aca75b53a091727c2062
marshall7m <44485531+marshall7m@users.noreply.github.com>
2022-03-14 16:23:35 -0700

    fix rollback provider sql alias syntax

--- buildspecs/trigger_sf/sql/update_executions_with_new_rollback_stack.sql
+++ buildspecs/trigger_sf/sql/update_executions_with_new_rollback_stack.sql
@@ -71,7 +71,7 @@ SELECT
     deploy_role_arn
 FROM (
     SELECT
-        'run-' || pr_id || '-' || substring(commit_id, 1, 4) || '-' || regexp_replace(cfg_path, '.*/', '') || '-'  || substr(md5(random()::text), 0, 4),
+        'run-' || pr_id || '-' || substring(commit_id, 1, 4) || '-' || regexp_replace(cfg_path, '.*/', '') || '-'  || substr(md5(random()::text), 0, 4) AS execution_id,
         true AS is_rollback,
         pr_id,
         commit_id,
