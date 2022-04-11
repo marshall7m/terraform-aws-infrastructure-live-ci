81f43669cef64590889451d487f9d0914b6cfdc4
marshall7m <44485531+marshall7m@users.noreply.github.com>
2022-04-11 11:54:44 -0700

    add working plan.sh

--- buildspecs/pr_plan/plan.sh
+++ buildspecs/pr_plan/plan.sh
@@ -5,25 +5,29 @@ for account in $(echo "$ACCOUNT_DIM" | jq -r -c '. | .[]'); do
     echo "$account"
     account_path=$(echo "$account" | jq -r '.path')
     echo "Account Path: $account_path"
-    head_ref=$(git branch -r --contains "$CODEBUILD_RESOLVED_SOURCE_VERSION" | xargs)
+    head_ref=$(git branch -r --contains "$CODEBUILD_RESOLVED_SOURCE_VERSION" | tr -d " ")
     echo "Remote Head Ref: $head_ref"
-    
     diff_filepaths=($(git diff "$CODEBUILD_WEBHOOK_BASE_REF" "$head_ref" --name-only --diff-filter=AM -- $account_path/**.tf $account_path/**.hcl))
-    diff_paths=()
-    for filepath in "${diff_filepaths[@]}"; do
-        diff_paths+=($(dirname $filepath))
-    done
-    
-    echo "New/Modified Paths:"
-    printf '%s\n' "${diff_paths[@]}"
+    echo "Filepath Count: ${#diff_filepaths[@]}"
+    if [ ${#diff_filepaths[@]} -gt 0 ]; then
+        diff_paths=()
+        for filepath in "${diff_filepaths[@]}"; do
+            diff_paths+=($(dirname $filepath))
+        done
 
-    echo "Diff Count: ${#diff_paths[@]}"
-    if [ ${#diff_paths[@]} -gt 0 ]; then
+        diff_paths=($(echo "${diff_paths[@]}" | tr ' ' '\n' | sort -u | tr '\n' ' '))
+        
+        echo "New/Modified Directories:"
+        printf '%s\n' "${diff_paths[@]}"
+        echo "Count: ${#diff_paths[@]}"
+
         plan_role_arn=$(echo "$account" | jq -r '.plan_role_arn')
         echo "Plan Role ARN: $plan_role_arn"
         for path in "${diff_paths[@]}"; do
             echo "Config Path: $path"
             terragrunt plan --terragrunt-working-dir "$path" --terragrunt-iam-role "$plan_role_arn"
         done
+    else
+        echo "No New/Modified Terraform configurations within account -- skipping Terraform plan"
     fi
 done
