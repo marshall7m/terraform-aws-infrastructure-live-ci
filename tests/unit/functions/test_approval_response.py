f452e3b03269c459ec5d2b384ebdd1483abfb98f
marshall7m <44485531+marshall7m@users.noreply.github.com>
2022-04-15 10:16:02 -0700

    add working testing approval response

--- tests/unit/functions/test_approval_response.py
+++ tests/unit/functions/test_approval_response.py
@@ -1,68 +1,158 @@
 import pytest
 import unittest
 import os
+import logging
+import sys
+import json
 from unittest.mock import patch
-import psycopg2
+from pprint import pformat
+from tests.helpers.utils import SetupUnit
+from psycopg2 import sql
 
-from approval_response.lambda_function import lambda_handler
+log = logging.getLogger(__name__)
+stream = logging.StreamHandler(sys.stdout)
+log.addHandler(stream)
+log.setLevel(logging.DEBUG)
 
-class TestApprovalResponse(unittest.TestCase):
+def run_lambda(event=None, context=None):
+    '''Imports Lambda function after boto3 client patch has been created to prevent boto3 region_name not specified error'''
+    from functions.approval_response.lambda_function import lambda_handler
+    return lambda_handler(event, context)
 
-    def append_first(self):
-        sf.send_task_success = unittest.MagicMock()
-        return None
-
-    @pytest.fixture(autouse=True)
-    def append_first(self):
-        os.environ["AWS_DEFAULT_REGION"] = "us-west-2"
-        return None
-
-    @pytest.fixture(autouse=True)
-    def context(self):
-        return {}
-
-    @pytest.fixtures
-    def event(self):
-        return {
-            "body": {
-                "action": "approval",
-                "recipient": "test-user"
+@pytest.mark.parametrize('event,record,status,expected_status_code,expected_send_token', [
+    pytest.param(
+        {
+            'body': {
+                'action': 'approve',
+                'recipient': 'test-user'
+            },
+            'query': {
+                'ex': 'mock-run',
+                'exId': 'mock-arn',
+                'taskToken': 'mock-token'
             }
-        }
-
-    def create_mock_event(body):
-        query = {
-            "query": {
-                "taskToken": "test-token",
-                "ex": "run-001"
+        },
+        {
+            'execution_id': 'mock-run',
+            'min_approval_count': 1,
+            'approval_voters': []
+        },
+        'RUNNING',
+        302,
+        True,
+        id='approval_met'
+    ),
+    pytest.param(
+        {
+            'body': {
+                'action': 'approve',
+                'recipient': 'test-user'
+            },
+            'query': {
+                'ex': 'mock-run',
+                'exId': 'mock-arn',
+                'taskToken': 'mock-token'
             }
-        }
-        return {**body, **query}
+        },
+        {
+            'execution_id': 'mock-run',
+            'min_approval_count': 2,
+            'approval_voters': []
+        },
+        'RUNNING',
+        302,
+        False,
+        id='approval_not_met'
+    ),
+    pytest.param(
+        {
+            'body': {
+                'action': 'approve',
+                'recipient': 'test-user'
+            },
+            'query': {
+                'ex': 'mock-run',
+                'exId': 'mock-arn',
+                'taskToken': 'mock-token'
+            }
+        },
+        {
+            'execution_id': 'mock-run',
+            'min_approval_count': 1,
+            'approval_voters': [],
+            'rejection_voters': ['test-user']
+        },
+        'RUNNING',
+        302,
+        False,
+        id='update_vote_to_approval'
+    ),
+    pytest.param(
+        {
+            'body': {
+                'action': 'approve',
+                'recipient': 'test-user'
+            },
+            'query': {
+                'ex': 'mock-run',
+                'exId': 'mock-arn',
+                'taskToken': 'mock-token'
+            }
+        },
+        {},
+        'ABORTED',
+        410,
+        False,
+        id='not_running'
+    ),
+    pytest.param(
+        {
+            'body': {
+                'action': 'approve',
+                'recipient': 'test-user'
+            },
+            'query': {
+                'ex': 'mock-run',
+                'exId': 'mock-arn',
+                'taskToken': 'mock-token'
+            }
+        },
+        {},
+        'RUNNING',
+        500,
+        False,
+        id='record_not_exists'
+    )
+])
+@patch('functions.approval_response.lambda_function.sf')
+@patch.dict(os.environ, {'METADB_CLUSTER_ARN': 'mock', 'METADB_SECRET_ARN': 'mock', 'METADB_NAME': 'mock'}, clear=True)
+@pytest.mark.usefixtures('mock_conn', 'aws_credentials')
+def test_lambda_handler(mock_sf, conn, cur, event, record, status, expected_status_code, expected_send_token):
+    mock_sf.describe_execution.return_value = {'status': status}
 
-    @patch("lambda_handler.boto3.client")
-    def test_valid_action(self, event, context):
-        response = lambda _handler(event, context)
-        expected = {
-            'statusCode': 302,
-            'message': 'Your choice has been submitted'
-        }
+    if record == {}:
+        log.info('Skip creating test record')
+    else:
+        log.info('Creating test record')
+        record = SetupUnit.create_records(conn, 'executions', record, enable_defaults=True)
+        log.info(f'Record: {pformat(record)}')
+    
+    log.info('Running Lambda Function')
+    response = run_lambda(event, {})
 
-        self.assertEqual(expected, response)
+    assert response['statusCode'] == expected_status_code
 
-    # def test_reject_action():
-    #     lambda_handler()
-
-    # def test_approve_action():
-    #     lambda_handler()
-
-    # def test_update_action():
-    #     lambda_handler()
-
-    # def test_already_submitted_action():
-    #     lambda_handler()
-
-    # def test_invalid_recipient_action():
-    #     lambda_handler()
-
-    # def test_count_reached_action():
-    #     lambda_handler()
+    if record != {}:
+        log.info("Assert record's approriate voters list contains the recipient")
+        cur.execute(sql.SQL('SELECT approval_voters, rejection_voters FROM executions WHERE execution_id = {}').format(sql.Literal(event['query']['ex'])))
+        res = dict(cur.fetchone())
+        
+        if event['body']['action'] == 'approve':
+            assert event['body']['recipient'] in res['approval_voters']
+            assert event['body']['recipient'] not in res['rejection_voters']
+        elif event['body']['action'] == 'reject':
+            assert event['body']['recipient'] in res['rejection_voters']
+            assert event['body']['recipient'] not in res['approval_voters']
+    
+    if expected_send_token:
+        mock_sf.send_task_success.assert_called_with(taskToken=event['query']['taskToken'], output=json.dumps(event['body']['action']))
