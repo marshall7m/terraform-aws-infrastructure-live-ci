53823bff088fdf185ee8452038cd9d5ac66921c2
marshall7m <44485531+marshall7m@users.noreply.github.com>
2022-04-16 13:57:26 -0700

    test private methods of trigger sf lambda to separate testing logic into different test methods

--- tests/unit/functions/test_trigger_sf.py
+++ tests/unit/functions/test_trigger_sf.py
@@ -15,53 +15,115 @@ import sys
 log = logging.getLogger(__name__)
 log.setLevel(logging.DEBUG)
 
-def lambda_handler(event=None, context=None):
-    '''Imports Lambda function after boto3 client patch has been created to prevent boto3 region_name not specified error'''
-    from functions.trigger_sf.lambda_function import lambda_handler
-    return lambda_handler(event, context)
-
-@pytest.mark.parametrize('event,records,merge_lock,expected_merge_lock,expected_running_ids', [
-    pytest.param(
-        {
-            'execution_id': '',
-            'status': '',
-            'cfg_path': '',
-            'is_rollback': '',
-            'commit_id': ''
-        },
+@patch('functions.trigger_sf.lambda_function.sf')
+@patch.dict(os.environ, {"METADB_CLUSTER_ARN": "mock","METADB_SECRET_ARN": "mock", "METADB_NAME": "mock", "STATE_MACHINE_ARN": "mock", 'GITHUB_MERGE_LOCK_SSM_KEY': 'mock-ssm-key'}, clear=True)
+@pytest.mark.usefixtures('aws_credentials')
+@pytest.mark.parametrize('records,execution,expected_aborted_ids,expected_rollback_cfg_paths', [
+    (
         [
             {
                 'execution_id': 'run-foo',
-                'commit_id': 'foo',
-                'is_rollback': 'false',
-                'status': 'failed',
-                'account_name': 'dev',
-                'account_deps': [],
-                'cfg_path': 'dev/foo',
-                'cfg_deps': []
+                'status': 'running',
+                'is_rollback': False,
+                'commit_id': 'test-commit'
             }
         ],
-        1,
-        1,
-        ['run-foo'],
-        id='successful_run'
+        {
+            'execution_id': 'run-foo',
+            'status': 'succeeded',
+            'is_rollback': False,
+            'commit_id': 'test-commit'
+        },
+        [],
+        []
+    ),
+    (
+        [
+            {
+                'execution_id': 'run-bar',
+                'status': 'waiting',
+                'is_rollback': False,
+                'commit_id': 'test-commit'
+            },
+            {
+                'execution_id': 'run-foo',
+                'status': 'running',
+                'is_rollback': False,
+                'commit_id': 'test-commit'
+            }
+        ],
+        {
+            'execution_id': 'run-foo',
+            'status': 'failed',
+            'is_rollback': False,
+            'commit_id': 'test-commit'
+        },
+        ['run-bar'],
+        []
+    ),
+    (
+        [
+            {
+                'execution_id': 'run-bar',
+                'account_name': 'dev',
+                'cfg_path': 'dev/bar',
+                'status': 'succeeded',
+                'is_rollback': False,
+                'commit_id': 'test-commit',
+                'new_providers': ['hashicorp/null'],
+                'new_resources': ['null_resource.this']
+            },
+            {
+                'execution_id': 'run-foo',
+                'status': 'running',
+                'is_rollback': False,
+                'commit_id': 'test-commit'
+            }
+        ],
+        {
+            'execution_id': 'run-foo',
+            'status': 'failed',
+            'is_rollback': False,
+            'commit_id': 'test-commit'
+        },
+        [],
+        ['dev/bar']
     )
 ])
+def test__execution_finished_status_update(mock_client, cur, conn, records, execution, expected_aborted_ids, expected_rollback_cfg_paths, insert_records):
+    from functions.trigger_sf import lambda_function
+    cur = conn.cursor()
+    mock_client.list_executions.return_value = {'executions': [{'name': record['execution_id'], 'executionArn': 'mock-arn'} for record in records if record['status'] != 'waiting']}
+    mock_client.stop_execution.return_value = None
+    records = insert_records('executions', records, enable_defaults=True)
+
+    response = lambda_function._execution_finished(cur, execution)
+
+    log.info('Assert finished execution record status was updated')
+    cur.execute(sql.SQL('SELECT status FROM executions WHERE execution_id = {}').format(sql.Literal(execution['execution_id'])))
+    assert execution['status'] == cur.fetchone()[0]
+
+    log.info('Assert Step Function executions were aborted')
+    cur.execute(sql.SQL("SELECT execution_id FROM executions WHERE commit_id = {} AND status = 'aborted'").format(sql.Literal(execution['commit_id'])))
+    res = [val[0] for val in cur.fetchall()]
+    log.debug(f'Actual: {res}')
+    assert all(path in res for path in expected_aborted_ids) == True 
+
+    log.info('Assert rollback execution records were created')
+    cur.execute(sql.SQL("SELECT cfg_path FROM executions WHERE commit_id = {} AND is_rollback = true").format(sql.Literal(execution['commit_id'])))
+    res = [val[0] for val in cur.fetchall()]
+    log.debug(f'Actual: {res}')
+    assert all(path in res for path in expected_rollback_cfg_paths) == True 
+
+@pytest.mark.skip(msg='Not implemented')
 @patch('functions.trigger_sf.lambda_function.sf')
-@patch('functions.trigger_sf.lambda_function.ssm')
 @patch.dict(os.environ, {"METADB_CLUSTER_ARN": "mock","METADB_SECRET_ARN": "mock", "METADB_NAME": "mock", "STATE_MACHINE_ARN": "mock", 'GITHUB_MERGE_LOCK_SSM_KEY': 'mock-ssm-key'}, clear=True)
 @pytest.mark.usefixtures('mock_conn', 'aws_credentials')
-def test_lambda_handler(mock_ssm, mock_sf, event, records, merge_lock, expected_merge_lock, expected_running_ids):
-
-    response = lambda_handler(event, {})
-    assert response == None
-    log.info('Assert finished execution record status was updated')
-    log.info('Assert Step Function executions were aborted')
-
+@patch('functions.trigger_sf.lambda_function.ssm')
+def test__start_executions(merge_lock, expected_merge_lock, expected_running_ids):
     log.info('Assert Step Function executions were started with approriate input')
     log.info('Assert started Step Function execution statuses were updated to running')
 
     log.info('Assert merge lock value was resetted')
 
-
-ids = ['test_failed_rollback', 'test_failed_execution', 'test_multiple_commmits_waiting']
+# ids = ['test_failed_rollback', 'test_failed_execution', 'test_multiple_commmits_waiting']
