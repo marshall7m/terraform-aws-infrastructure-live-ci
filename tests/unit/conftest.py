09d708a59cdcc09b2d731ce32f1bff766ef4025e
marshall7m <44485531+marshall7m@users.noreply.github.com>
2022-04-17 18:09:27 -0700

    add unit testing specific psycopg2 conn related fixt

---
+++ tests/unit/conftest.py
@@ -0,0 +1,58 @@
+import pytest
+import psycopg2
+from psycopg2 import sql
+import os
+import timeout_decorator
+import sys
+import logging
+import psycopg2.extras
+
+log = logging.getLogger(__name__)
+log.setLevel(logging.DEBUG)
+
+@pytest.fixture(scope="session", autouse=True)
+def conn():
+    '''psycopg2 connection with auto commit set to True'''
+    conn = psycopg2.connect(connect_timeout=10)
+    conn.set_session(autocommit=True)
+
+    yield conn
+    conn.close()
+
+@pytest.fixture(scope='session', autouse=True)
+def cur(conn):
+    '''psycopg2 cursor that returns dictionary type results {column_name: value}'''
+    cur = conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+    yield cur
+    cur.close()
+
+@timeout_decorator.timeout(30)
+@pytest.fixture(scope='session', autouse=True)
+def setup_metadb(cur):
+    '''Creates `account_dim` and `executions` table'''
+    log.info('Creating metadb tables')
+    with open(f'{os.path.dirname(os.path.realpath(__file__))}/../../sql/create_metadb_tables.sql', 'r') as f:
+        cur.execute(sql.SQL(f.read().replace('$', '')).format(metadb_schema=sql.Identifier('public'), metadb_name=sql.Identifier(os.environ['PGDATABASE'])))
+
+@pytest.fixture(scope='function', autouse=True)
+def truncate_executions(cur):
+    '''Removes all rows from execution table after every test'''
+    log.info('Truncating executions table')
+    cur.execute("TRUNCATE executions")
+
+@pytest.fixture(autouse=True)
+def mock_conn(mocker, conn):
+    '''Patches AWS RDS client with psycopg2 client that connects to the local docker container Postgres database'''
+    mocker.patch('aurora_data_api.connect', return_value=conn)
+
+@pytest.fixture(scope='function')
+def aws_credentials():
+    '''
+    Mocked AWS credentials needed to be set before importing Lambda Functions that define global boto3 clients. 
+    This prevents the region_name not specified errors.
+    '''
+    os.environ['AWS_ACCESS_KEY_ID'] = 'testing'
+    os.environ['AWS_SECRET_ACCESS_KEY'] = 'testing'
+    os.environ['AWS_SECURITY_TOKEN'] = 'testing'
+    os.environ['AWS_SESSION_TOKEN'] = 'testing'
+    os.environ['AWS_DEFAULT_REGION'] = 'us-west-2'
