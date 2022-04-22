463ea25778428a5a1187c1d0edaa06b98e8da769
marshall7m <44485531+marshall7m@users.noreply.github.com>
2022-04-22 08:25:37 -0700

    rm terragrunt binary and let tgswitch manage version

--- install.sh
+++ install.sh
@@ -14,7 +14,6 @@ python3 -m pip install -r requirements.txt
 wget -q -O /tmp/terraform.zip https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip
 unzip -q /tmp/terraform.zip
 mv $(unzip -qql /tmp/terraform.zip | head -n1 | tr -s ' ' | cut -d' ' -f5-) /usr/local/bin/
-wget -q -O /usr/local/bin/terragrunt https://github.com/gruntwork-io/terragrunt/releases/download/v${TERRAGRUNT_VERSION}/terragrunt_linux_amd64
 wget -q -O /tmp/tflint.zip https://github.com/terraform-linters/tflint/releases/download/v${TFLINT_VERSION}/tflint_linux_amd64.zip
 unzip -q /tmp/tflint.zip
 mv $(unzip -qql /tmp/tflint.zip | head -n1 | tr -s ' ' | cut -d' ' -f5-) /usr/local/bin/
