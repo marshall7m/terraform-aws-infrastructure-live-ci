d50424daf1eab8101a96b0bd6a7573f245d77a4e
marshall7m <44485531+marshall7m@users.noreply.github.com>
2022-04-29 10:17:26 -0700

    add initial tf module unit test for checking defaults

---
+++ tests/unit/tf-module-defaults/test_defaults.py
@@ -0,0 +1,25 @@
+import os
+import logging
+import tftest
+import pytest
+import sys
+
+log = logging.getLogger(__name__)
+stream = logging.StreamHandler(sys.stdout)
+log.addHandler(stream)
+log.setLevel(logging.DEBUG)
+
+def test_defaults():
+    tf_dir = os.path.dirname(os.path.realpath(__file__))
+
+    if 'GITHUB_TOKEN' not in os.environ:
+        pytest.fail('$GITHUB_TOKEN env var is not set -- required to setup Github resources')
+
+    tf = tftest.TerraformTest(tf_dir)
+
+    # log.info('Initializing testing module')
+    # tf.init()
+
+    log.info('Getting testing tf plan')
+    out = tf.plan(output=True)
+    log.debug(out)
