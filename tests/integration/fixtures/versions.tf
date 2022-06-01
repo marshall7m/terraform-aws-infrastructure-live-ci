b8ecefeff95df61258fa4ec6a7c9c3a4ee180f08
marshall7m <44485531+marshall7m@users.noreply.github.com>
2022-06-01 15:09:53 -0700

    rm not needed tf providers from integration tests fixt

--- tests/integration/fixtures/versions.tf
+++ tests/integration/fixtures/versions.tf
@@ -3,26 +3,15 @@ terraform {
   required_providers {
     random = {
       source  = "hashicorp/random"
-      version = "3.1.0"
+      version = ">=3.1.0"
     }
     null = {
       source  = "hashicorp/null"
       version = ">=3.1.0"
     }
-    test = {
-      source = "terraform.io/builtin/test"
-    }
     github = {
       source  = "integrations/github"
       version = "4.9.3"
-    }
-    testing = {
-      source  = "apparentlymart/testing"
-      version = "0.0.2"
-    }
-    tls = {
-      source  = "hashicorp/tls"
-      version = "3.1.0"
     }
   }
 }
