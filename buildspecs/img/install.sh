c729da24cf2359a2be23260f36aae86f2a17803c
marshall7m <44485531+marshall7m@users.noreply.github.com>
2022-06-07 13:04:34 -0700

    replace 3.9-alpine with 3.9-slim-buster for both stages of ghcr image

--- buildspecs/img/install.sh
+++ buildspecs/img/install.sh
@@ -2,6 +2,11 @@
 # shellcheck disable=SC1091
 # SC1091: $VIRTUAL_ENV will be exported from the Dockerfile
 
+set -e
+
+apt-get -y update
+apt-get install -y wget
+
 python -m venv "$VIRTUAL_ENV"
 source "$VIRTUAL_ENV"/bin/activate
 python3 -m pip install --upgrade pip
@@ -14,4 +19,6 @@ mkdir /usr/local/.tfenv && mv /tmp/tfenv-"${TFENV_VERSION}"/* /usr/local/.tfenv 
 wget -q -O - https://raw.githubusercontent.com/warrensbox/tgswitch/release/install.sh | bash -s -- -b /usr/local/bin -d "${TGSWITCH_VERSION}"
 
 chmod u+x /usr/local/bin/*
+apt-get clean
+rm -rf /var/lib/apt/lists/*
 rm -rf /tmp/*
