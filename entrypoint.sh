d36b3f04f91df5d5386c7447bed6b39aa34d9da8
marshall7m <44485531+marshall7m@users.noreply.github.com>
2022-06-08 13:29:40 -0700

    add shellcheck and sqlfluff install to testing img entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,19 @@ if [ -n "$ADDITIONAL_PATH" ]; then
     export PATH="$ADDITIONAL_PATH:$PATH"
 fi
 
+scversion="stable"
+echo "Installing shellcheck - ${scversion}"
+
+apt-get install -y xz-utils
+curl -L -o /tmp/shellcheck-${scversion?}.linux.x86_64.tar.xz https://github.com/koalaman/shellcheck/releases/download/${scversion?}/shellcheck-${scversion?}.linux.x86_64.tar.xz
+tar -xf /tmp/shellcheck-${scversion?}.linux.x86_64.tar.xz
+cp "/tmp/shellcheck-${scversion}/shellcheck" /usr/local/bin/
+shellcheck --version
+
+apt-get clean
+rm -rf /var/lib/apt/lists/*
+rm -rf /tmp/*
+
 source "$VIRTUAL_ENV"/bin/activate
 python3 -m pip install -r requirements.txt
 python3 -m pip install -e /src
