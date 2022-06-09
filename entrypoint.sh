5a619f63f3c8c715523944aae3be4d92a6e14e73
marshall7m <44485531+marshall7m@users.noreply.github.com>
2022-06-09 09:05:37 -0700

    rm shellcheck source install and install via pip shellcheck-py rm requirements.txt and specify testing env level dependencies within setup.py

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,19 +7,6 @@ if [ -n "$ADDITIONAL_PATH" ]; then
     export PATH="$ADDITIONAL_PATH:$PATH"
 fi
 
-scversion="stable"
-echo "Installing shellcheck - ${scversion}"
-
-apt-get install -y xz-utils
-curl -L -s -o /tmp/shellcheck-${scversion?}.linux.x86_64.tar.xz https://github.com/koalaman/shellcheck/releases/download/${scversion?}/shellcheck-${scversion?}.linux.x86_64.tar.xz
-tar -xf /tmp/shellcheck-${scversion?}.linux.x86_64.tar.xz -C /tmp
-cp "/tmp/shellcheck-${scversion}/shellcheck" /usr/local/bin/
-shellcheck --version
-
-apt-get clean
-rm -rf /var/lib/apt/lists/*
-rm -rf /tmp/*
-
 source "$VIRTUAL_ENV"/bin/activate
 python3 -m pip install -r requirements.txt
 python3 -m pip install -e /src
--- setup.py
+++ setup.py
@@ -1,3 +1,15 @@
 from setuptools import setup, find_packages
 
-setup(name="terraform-aws-infrastructure-live", packages=find_packages())
+extras_require = {
+    "precommit": ["sqlfluff", "shellcheck-py"],
+    "unit": ["GitPython", "psycopg2-binary", "awscli", "boto3"],
+    "integration": ["aurora-data-api", "GitPython", "awscli", "boto3"],
+}
+extras_require["all"] = list(extras_require.values())
+
+setup(
+    name="terraform-aws-infrastructure-live",
+    packages=find_packages(),
+    extras_require=extras_require,
+    description="Setup for testing environments",
+)
