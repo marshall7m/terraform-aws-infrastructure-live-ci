abb80b60dd4761ee4f348c5ae790a68b0d28e541
marshall7m <44485531+marshall7m@users.noreply.github.com>
2022-06-30 20:13:38 -0700

    use explicit SOURCE_REPO_PATH env var and add TODO for ecs cmd guardrails

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+set -e
+
 if [ -n "${TERRAFORM_VERSION}" ]; then
     echo "Installing Terraform version via tfenv: ${TERRAFORM_VERSION}"
     tfenv install "${TERRAFORM_VERSION}"
@@ -12,11 +15,17 @@ if [ -n "${TERRAGRUNT_VERSION}" ]; then
 fi
 echo "Terragrunt Version: $(terragrunt --version)"
 
-echo "Cloning source repo URL: $SOURCE_CLONE_URL"
-# TODO: cd into source repo
-git clone -b "$SOURCE_VERSION" --single-branch "$SOURCE_CLONE_URL" source-repo
-cd source-repo || exit 1
+export SOURCE_REPO_PATH="$PWD/source-repo"
+echo "Cloning source repo URL: $SOURCE_CLONE_URL to path: $SOURCE_REPO_PATH"
 
-echo "PWD: $PWD"
+git clone -b "$SOURCE_VERSION" --single-branch "$SOURCE_CLONE_URL" "$SOURCE_REPO_PATH"
+cd "$SOURCE_REPO_PATH" || exit 1
 
+echo "Current working directory: $PWD"
+
+# TODO: Design guardrails around what commands can be runned from the ecs task
+# Either restrict commands only from this docker image's /src (user won't be able to customize task execution with before/after scripts)
+# or create a check to ensure that the command is only running a script from the $SOURCE_CLONE_URL
 exec "$@"
+
+# /bin/bash -c
