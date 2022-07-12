6059d7aee33b7ae4a3f22dd3de7538262b0d4431
marshall7m <44485531+marshall7m@users.noreply.github.com>
2022-07-12 11:36:42 -0700

    add expect_failed_create_deploy_stack to TestInvalidTFConfig

--- tests/integration/test_invalid_tf_cfg.py
+++ tests/integration/test_invalid_tf_cfg.py
@@ -7,13 +7,13 @@ class TestInvalidTFConfig(test_integration.Integration):
     """
     Case covers a simple 2 node deployment with one node having an account-level dependency on the other.
     See the account_dim table to see the account dependency testing layout.
-    The error caused by the invalid Terraform configuration(s) within the dev-account should cause the create_deploy_stack.py script to
-    rollback the shared-services account's execution records and fail the build entirely without any other downstream services being invoked.
+    The error caused by the invalid Terraform configuration(s) within the dev-account should cause the Create Deploy Stack task to
+    rollback the shared-services account's execution records and fail the task entirely without any other downstream services being invoked.
     """
 
     case = {
         "head_ref": f"feature-{uuid.uuid4()}",
-        "expect_failed_pr_plan": True,
+        "expect_failed_create_deploy_stack": True,
         "executions": {
             "directory_dependency/shared-services-account/us-west-2/env-one/doo": {
                 "pr_files_content": [dummy_tf_output()]
