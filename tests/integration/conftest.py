2dc2fbfd20ebe5c8bfe90c3478a0ea928d6a4a26
Marshall Mamiya <44485531+marshall7m@users.noreply.github.com>
2022-09-01 10:14:13 -0700

    add integration specific tf/terraform_version fixt

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -1,14 +1,39 @@
 import pytest
 import requests
+import os
 import logging
 
 log = logging.getLogger(__name__)
 log.setLevel(logging.DEBUG)
 
+
 def pytest_addoption(parser):
     parser.addoption(
         "--skip-moto-reset", action="store_true", help="skips resetting moto server"
     )
+
+
+def pytest_generate_tests(metafunc):
+    tf_versions = [pytest.param("latest")]
+    if "terraform_version" in metafunc.fixturenames:
+        tf_versions = [pytest.param("latest")]
+        metafunc.parametrize(
+            "terraform_version",
+            tf_versions,
+            indirect=True,
+            scope="session",
+            ids=[f"tf_{v.values[0]}" for v in tf_versions],
+        )
+
+    if "tf" in metafunc.fixturenames:
+        metafunc.parametrize(
+            "tf",
+            [f"{os.path.dirname(__file__)}/fixtures"],
+            indirect=True,
+            scope="session",
+        )
+
+
 @pytest.fixture(scope="session")
 def reset_moto_server(request):
     yield None
