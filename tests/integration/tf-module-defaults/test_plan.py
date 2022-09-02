bbca9e487066a84dad4b8ea7534c5b100bc18f12
Marshall Mamiya <44485531+marshall7m@users.noreply.github.com>
2022-09-01 21:58:49 -0700

    mv tf plan test to tests/integration/

--- tests/integration/tf-module-defaults/test_plan.py
+++ tests/integration/tf-module-defaults/test_plan.py
@@ -32,3 +32,27 @@ def test_plan(tf, terraform_version, repo):
     log.info("Getting testing tf plan")
     out = tf.plan(output=True)
     log.debug(out)
+
+
+@pytest.mark.parametrize(
+    "terraform_version,tf,repo",
+    [
+        (
+            "1.0.0",
+            f"{os.path.dirname(os.path.realpath(__file__))}/fixtures",
+            f"mut-terraform-aws-infrastructure-live-{uuid.uuid4()}",
+        )
+    ],
+    indirect=True,
+)
+def test_apply(tf, terraform_version, repo):
+    """
+    Ensure that can be successfully applied with just
+    the module's required variables defined
+    """
+    # add TF_VARS to tf obj env so TF_VARS are available for tf destroy cmd
+    tf.env.update({"TF_VAR_repo_name": repo.full_name})
+
+    log.info("Running Terraform apply")
+    out = tf.apply(auto_approve=True)
+    log.debug(out)
