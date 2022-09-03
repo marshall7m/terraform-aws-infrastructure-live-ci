38a2e1e120e06459128d9c8a19187f662a1984ae
Marshall Mamiya <44485531+marshall7m@users.noreply.github.com>
2022-09-03 12:17:42 -0700

    add terra fixt to tf plan pytest

--- tests/integration/tf-module-defaults/test_plan.py
+++ tests/integration/tf-module-defaults/test_plan.py
@@ -2,7 +2,6 @@ import os
 import logging
 import pytest
 import sys
-import uuid
 
 log = logging.getLogger(__name__)
 stream = logging.StreamHandler(sys.stdout)
@@ -10,49 +9,39 @@ log.addHandler(stream)
 log.setLevel(logging.DEBUG)
 
 
-@pytest.mark.parametrize(
-    "terraform_version,tf,repo",
-    [
-        (
-            "1.0.0",
-            f"{os.path.dirname(os.path.realpath(__file__))}/fixtures",
-            f"mut-terraform-aws-infrastructure-live-{uuid.uuid4()}",
+def pytest_generate_tests(metafunc):
+    if "terra" in metafunc.fixturenames:
+        metafunc.parametrize(
+            "terra",
+            [
+                pytest.param(
+                    {
+                        "binary": "terragrunt",
+                        "command": "plan",
+                        "skip_teardown": False,
+                        "env": {"IS_REMOTE": "False"},
+                        "tfdir": f"{os.path.dirname(os.path.realpath(__file__))}/../../fixtures/terraform/mut/defaults",
+                    },
+                    id="defaults",
+                )
+            ],
+            indirect=True,
+            scope="module",
         )
-    ],
-    indirect=True,
-)
-def test_plan(tf, terraform_version, repo):
+
+
+def test_plan(terra):
     """
     Ensure that the Terraform module produces a valid Terraform plan with just
     the module's required variables defined
     """
-    # add TF_VARS to tf obj env so TF_VARS are available for tf destroy cmd
-    tf.env.update({"TF_VAR_repo_name": repo.full_name})
-
-    log.info("Getting testing tf plan")
-    out = tf.plan(output=True)
-    log.debug(out)
+    log.info("Terraform plan:")
+    log.debug(terra)
 
 
-@pytest.mark.parametrize(
-    "terraform_version,tf,repo",
-    [
-        (
-            "1.0.0",
-            f"{os.path.dirname(os.path.realpath(__file__))}/fixtures",
-            f"mut-terraform-aws-infrastructure-live-{uuid.uuid4()}",
-        )
-    ],
-    indirect=True,
-)
-def test_apply(tf, terraform_version, repo):
+def test_apply(terra):
     """
     Ensure that can be successfully applied with just
     the module's required variables defined
     """
-    # add TF_VARS to tf obj env so TF_VARS are available for tf destroy cmd
-    tf.env.update({"TF_VAR_repo_name": repo.full_name})
-
-    log.info("Running Terraform apply")
-    out = tf.apply(auto_approve=True)
-    log.debug(out)
+    pass
