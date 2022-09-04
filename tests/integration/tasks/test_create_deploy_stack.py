cc2fac4d61141d63ea3d335b979e4d6be7c556a0
Marshall Mamiya <44485531+marshall7m@users.noreply.github.com>
2022-09-04 14:49:27 -0700

    rm brain dump & add terra fixt param

--- tests/integration/tasks/test_create_deploy_stack.py
+++ tests/integration/tasks/test_create_deploy_stack.py
@@ -28,53 +28,48 @@ tests:
     - assert trigger sf was called
 - failed
     - assert rollback records
-
-
-
-Terragrunt:
-generate providers:
-    - local moto urls
-    - real AWS
-
-terragrunt mut module source
-
-module deps:
-    vpc
-    git repo
-    policies
-    tf state bucket
-
-put generate block in parent tg file
-
-fixtures/
-    terraform/
-        deps/
-            standard/
-                deps.tf
-        local/
-            terragrunt.hcl (provider generate block)
-            deps/
-                terragrunt.hcl
-                
-            mut/
-                with deps/
-                    dependency on ../deps
-                    terragrunt.hcl
-                defaults/
-                    terragrunt.hcl
-        remote/
-
+LOCAL_ECS_IMAGE_ADDRESS
 """
 
+def pytest_generate_tests(metafunc):
+    if "terra" in metafunc.fixturenames:
+        metafunc.parametrize("terra", [
+            pytest.param(
+                {
+                    "binary": "terragrunt",
+                    "skip_teardown": True,
+                    "env": {
+                        "IS_REMOTE": "False",
+                        "TF_VAR_ecs_image_address": os.environ["LOCAL_ECS_IMAGE_ADDRESS"],
+                        "TF_VAR_approval_sender_arn": "arn:aws:ses:us-west-2:123456789012:identity/fakesender@fake.com",
+                        "TF_VAR_approval_request_sender_email": "fakesender@fake.com",
+                        "TF_VAR_create_approval_sender_policy": "false"
+                    },
+                    "tfdir": f"{os.path.dirname(os.path.realpath(__file__))}/../../fixtures/terraform/mut/basic"
+                }
+            )
+        ], indirect=True, scope="module"
+    )
+
+@pytest.fixture
+def mut(terra):
+    terra.setup(cleanup_on_exit=False)
+    terra.run("apply", get_cache=True, extra_args={"auto_approve": True})
+    return terra
 
 @pytest.mark.usefixtures("truncate_executions")
 @pytest.mark.parametrize("changes", [{"foo/a.tf": dummy_tf_output()}])
-def test_stack(tmp_path, mut_output, repo, changes):
-    push(repo, BASE_REF, changes)
+def test_stack(mut, tmp_path, changes):
+
+    output = mut.run("output", get_cache=True)
+    log.debug(output)
+    # push(repo, BASE_REF, changes)
 
     task_def = ecs.describe_task_definition(
-        taskDefinition=mut_output["create_deploy_stack_task_definition_arn"]
+        taskDefinition=output["ecs_create_deploy_stack_definition_arn"]
     )["taskDefinition"]
+
+    log.debug(task_def)
 
     task_def_filepath = tmp_path / "task_def.json"
     task_def_filepath.write_text(task_def)
@@ -84,28 +79,29 @@ def test_stack(tmp_path, mut_output, repo, changes):
     )
 
     compose = conv.convert()
-
+    log.debug(compose)
     compose_filepath = tmp_path / "docker-compose.yml"
+    compose_filepath.write_text(compose)
 
     docker = DockerClient(compose_files=[compose_filepath])
 
-    out = docker.compose.execute(
-        "create-deploy-stack",
-        envs={
-            "SOURCE_CLONE_URL": repo.clone_url,
-            "REPO_FULL_NAME": repo.full_name,
-            "GITHUB_MERGE_LOCK_SSM_KEY": merge_lock,
-            "SOURCE_VERSION": BASE_REF,
-            "STATUS_CHECK_NAME": "CreateDeployStack",
-            "TRIGGER_SF_FUNCTION_NAME": trigger_sf,
-            "METADB_NAME": os.environ["METADB_NAME"],
-            "AURORA_CLUSTER_ARN": os.environ["AURORA_CLUSTER_ARN"],
-            "AURORA_SECRET_ARN": os.environ["AURORA_SECRET_ARN"],
-            "BASE_REF": BASE_REF,
-            "HEAD_REF": "feature-123",
-            "PR_ID": "1",
-            "COMMIT_ID": repo.get_branch(BASE_REF).commit.sha,
-        },
-    )
+    # out = docker.compose.execute(
+    #     "create-deploy-stack",
+    #     envs={
+    #         "SOURCE_CLONE_URL": repo.clone_url,
+    #         "REPO_FULL_NAME": repo.full_name,
+    #         "GITHUB_MERGE_LOCK_SSM_KEY": merge_lock,
+    #         "SOURCE_VERSION": BASE_REF,
+    #         "STATUS_CHECK_NAME": "CreateDeployStack",
+    #         "TRIGGER_SF_FUNCTION_NAME": trigger_sf,
+    #         "METADB_NAME": os.environ["METADB_NAME"],
+    #         "AURORA_CLUSTER_ARN": os.environ["AURORA_CLUSTER_ARN"],
+    #         "AURORA_SECRET_ARN": os.environ["AURORA_SECRET_ARN"],
+    #         "BASE_REF": BASE_REF,
+    #         "HEAD_REF": "feature-123",
+    #         "PR_ID": "1",
+    #         "COMMIT_ID": repo.get_branch(BASE_REF).commit.sha,
+    #     },
+    # )
 
     log.debug(out)
