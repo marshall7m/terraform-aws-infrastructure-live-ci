89b736a86e99aa3714e8db18c519baa725544830
Marshall Mamiya <44485531+marshall7m@users.noreply.github.com>
2022-09-15 14:50:59 -0700

    rm --preclean-terra --skip-tf-apply pytest flags

--- tests/integration/tasks/conftest.py
+++ tests/integration/tasks/conftest.py
@@ -1,9 +1,6 @@
 import os
 import pytest
 import logging
-import shutil
-import glob
-import requests
 
 log = logging.getLogger(__name__)
 log.setLevel(logging.DEBUG)
@@ -30,65 +27,13 @@ def pytest_generate_tests(metafunc):
             indirect=True,
             scope="session",
         )
+    if "terra_setup" in metafunc.fixturenames:
+        metafunc.parametrize(
+            "terra_setup", [{"cleanup_on_exit": False}], indirect=True, scope="session"
+        )
+        # metafunc.parametrize("terra_setup", [{"cleanup_on_exit": os.environ.get("IS_REMOTE") == None}], indirect=True, scope="session")
 
 
-def pytest_addoption(parser):
-    group = parser.getgroup("Terra-Fixt")
-
-    group.addoption(
-        "--skip-tf-init",
-        action="store_true",
-        help="skips initing Terraform configuration",
-    )
-
-    group.addoption(
-        "--skip-tf-apply",
-        action="store_true",
-        help="skips applying Terraform configuration",
-    )
-
-    group.addoption(
-        "--preclean-terra",
-        action="store_true",
-        help="Destroys Terraform resources before any other subsequent terra* commands",
-    )
-
-
-@pytest.fixture(scope="session")
-def mut(request, terra):
-    if request.config.getoption("preclean_terra"):
-        log.info("Destroying leftover Terraform resources before tests")
-        if os.environ.get("IS_REMOTE", False):
-            log.info("Running terraform destroy")
-            terra.destroy(auto_approve=True)
-        else:
-            log.info("Removing .terra* and tfstate files")
-            log.debug(f"Terra directory: {terra.tfdir}")
-
-            path = os.path.join(terra.tfdir, ".terraform")
-
-            if os.path.isdir(path):
-                shutil.rmtree(path)
-            path = os.path.join(terra.tfdir, "terraform.tfstate")
-
-            if os.path.isfile(path):
-                os.unlink(path)
-
-            path = os.path.join(terra.tfdir, "**", ".terragrunt-cache*")
-            for tg_dir in glob.glob(path, recursive=True):
-                if os.path.isdir(tg_dir):
-                    shutil.rmtree(tg_dir)
-
-            log.info("Resetting moto server")
-            requests.post(f"{os.environ['MOTO_ENDPOINT_URL']}/moto-api/reset")
-
-    if not request.config.getoption("skip_tf_init"):
-        terra.init()
-
-    if not request.config.getoption("skip_tf_apply"):
-        try:
-            terra.apply(auto_approve=True)
-        except Exception as e:
-            log.error(e, exc_info=True)
-
-    return terra
+@pytest.fixture()
+def mut_output(terra_apply, terra_output):
+    return {k: v["value"] for k, v in terra_output.items()}
