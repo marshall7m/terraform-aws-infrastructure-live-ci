3d3439a4f2d9b7166ba65e7a2bfbc9d54d9b7c6f
Marshall Mamiya <44485531+marshall7m@users.noreply.github.com>
2022-10-07 19:36:55 -0700

    replace terra fixt with tftest cache feat

--- tests/integration/tasks/conftest.py
+++ tests/integration/tasks/conftest.py
@@ -1,45 +1,33 @@
 import os
-import pytest
 import logging
 import uuid
+
+import pytest
 import github
+import tftest
 from tests.helpers.utils import push
 
 log = logging.getLogger(__name__)
 log.setLevel(logging.DEBUG)
 
 
-def pytest_generate_tests(metafunc):
-    if "terra" in metafunc.fixturenames:
-        metafunc.parametrize(
-            "terra",
-            [
-                pytest.param(
-                    {
-                        "binary": "terragrunt",
-                        "skip_teardown": True,
-                        "env": {
-                            "TF_VAR_approval_sender_arn": "arn:aws:ses:us-west-2:123456789012:identity/fakesender@fake.com",
-                            "TF_VAR_approval_request_sender_email": "fakesender@fake.com",
-                            "TF_VAR_create_approval_sender_policy": "false",
-                        },
-                        "tfdir": f"{os.path.dirname(os.path.realpath(__file__))}/../../fixtures/terraform/mut/basic",
-                    },
-                )
-            ],
-            indirect=True,
-            scope="session",
-        )
-    if "terra_setup" in metafunc.fixturenames:
-        metafunc.parametrize(
-            "terra_setup", [{"cleanup_on_exit": False}], indirect=True, scope="session"
-        )
-        # metafunc.parametrize("terra_setup", [{"cleanup_on_exit": os.environ.get("IS_REMOTE") == None}], indirect=True, scope="session")
-
-
 @pytest.fixture()
-def mut_output(terra_apply, terra_output):
-    return {k: v["value"] for k, v in terra_output.items()}
+def mut_output():
+    tf = tftest.TerragruntTest(
+        binary="terragrunt",
+        env={
+            "TF_VAR_approval_sender_arn": "arn:aws:ses:us-west-2:123456789012:identity/fakesender@fake.com",
+            "TF_VAR_approval_request_sender_email": "fakesender@fake.com",
+            "TF_VAR_create_approval_sender_policy": "false",
+        },
+        tfdir=f"{os.path.dirname(os.path.realpath(__file__))}/../../fixtures/terraform/mut/basic",
+        enable_cache=True,
+    )
+
+    tf.setup(cleanup_on_exit=True, use_cache=True)
+    tf.apply(auto_approve=True, use_cache=True)
+
+    return {k: v["value"] for k, v in tf.output(use_cache=True).items()}
 
 
 @pytest.fixture
