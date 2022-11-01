72a503ea417ba5a1317aea743103ad706bda25f3
Marshall Mamiya <44485531+marshall7m@users.noreply.github.com>
2022-10-31 21:38:21 -0700

    remove terra fixt from test_plan

--- tests/integration/tf-module/test_tf.py
+++ tests/integration/tf-module/test_tf.py
@@ -1,34 +1,28 @@
 import os
 import logging
-import pytest
+
+import tftest
 
 log = logging.getLogger(__name__)
 log.setLevel(logging.DEBUG)
 
-muts = []
+FILE_DIR = os.path.dirname(__file__)
 
 
-@pytest.mark.parametrize(
-    "terra",
-    [
-        pytest.param(
-            {
-                "binary": "terragrunt",
-                "command": "plan",
-                "skip_teardown": False,
-                "env": {"IS_REMOTE": "False"},
-                "tfdir": f"{os.path.dirname(os.path.realpath(__file__))}/../../fixtures/terraform/mut/defaults",
-            },
-            marks=pytest.mark.skip(),
-            id="defaults",
-        ),
-    ],
-    indirect=True,
-)
-def test_plan(terra):
+def test_plan(request):
     """
     Ensure that the Terraform module produces a valid Terraform plan with just
     the module's required variables defined
     """
-    log.info("Terraform plan:")
-    log.debug(terra)
+    cache_dir = str(request.config.cache.makedir("tftest"))
+    log.info(f"Caching Tftest results to {cache_dir}")
+
+    tf = tftest.TerragruntTest(
+        tfdir=os.path.join(FILE_DIR, "../../fixtures/terraform/mut/defaults"),
+        enable_cache=False,
+        cache_dir=cache_dir,
+        env={"IS_REMOTE": "False"},
+    )
+
+    tf.setup(cleanup_on_exit=True)
+    tf.plan(output=True)
