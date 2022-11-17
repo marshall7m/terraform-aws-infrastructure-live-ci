f966076a7ff214714f367807371d8a421aa91445
Marshall Mamiya <44485531+marshall7m@users.noreply.github.com>
2022-11-17 07:57:17 -0800

    fix rds data client config in get_commit_cfg_paths

--- tests/integration/tasks/test_create_deploy_stack.py
+++ tests/integration/tasks/test_create_deploy_stack.py
@@ -1,7 +1,6 @@
 import os
 import uuid
 import logging
-import json
 
 import pytest
 import boto3
@@ -13,25 +12,23 @@ log = logging.getLogger(__name__)
 log.setLevel(logging.DEBUG)
 
 gh = github.Github(os.environ["GITHUB_TOKEN"], retry=3)
-rds_data = boto3.client("rds-data", endpoint_url=os.environ["METADB_ENDPOINT_URL"])
+rds_data_client = boto3.client(
+    "rds-data", endpoint_url=os.environ["METADB_ENDPOINT_URL"]
+)
 
 
-def get_commit_cfg_paths(commit_id, mut_output):
+def get_commit_cfg_paths(commit_id: str):
     with aurora_data_api.connect(
-        aurora_cluster_arn=mut_output["metadb_arn"],
-        secret_arn=mut_output["metadb_secret_manager_master_arn"],
-        database=mut_output["metadb_name"],
-        rds_data_client=rds_data,
-    ) as conn:
-        with conn.cursor() as cur:
-            cur.execute(
-                f"""
-            SELECT array_agg(cfg_path::TEXT)
-            FROM {mut_output["metadb_schema"]}.executions
-            WHERE commit_id = '{commit_id}'
-            """
-            )
-            results = cur.fetchone()[0]
+        database=os.environ["METADB_NAME"], rds_data_client=rds_data_client
+    ) as conn, conn.cursor() as cur:
+        cur.execute(
+            f"""
+        SELECT array_agg(cfg_path::TEXT)
+        FROM executions
+        WHERE commit_id = '{commit_id}'
+        """
+        )
+        results = cur.fetchone()[0]
 
     return [] if results is None else [id for id in results]
 
@@ -50,47 +47,6 @@ def send_commit_status(push_changes, mut_output):
         context=mut_output["create_deploy_stack_status_check_name"],
         target_url="http://localhost:8080",
     )
-
-
-@pytest.fixture(scope="class")
-def remove_iam_role(mut_output):
-    """Removes assumable IAM role from service"""
-    iam = boto3.client("iam")
-    iam_res = boto3.resource("iam")
-
-    log.info("Creating deny policy")
-    response = iam.create_policy(
-        PolicyName=f'{mut_output["ecs_create_deploy_stack_family"]}-test-error',
-        PolicyDocument=json.dumps(
-            {
-                "Version": "2012-10-17",
-                "Statement": [
-                    {
-                        "Effect": "Deny",
-                        "Action": "sts:AssumeRole",
-                        "Resource": mut_output["plan_role_arn"],
-                    }
-                ],
-            }
-        ),
-        Description="Overwrites the services ability to assume the specified role",
-    )
-
-    arn = response["Policy"]["Arn"]
-    role = iam_res.Role(mut_output["ecs_create_deploy_stack_family"])
-
-    log.debug(
-        f'Attaching policy to role: {mut_output["ecs_create_deploy_stack_family"]}'
-    )
-    role.attach_policy(PolicyArn=arn)
-
-    yield arn
-
-    log.info("Detaching Deny policy")
-    response = role.detach_policy(PolicyArn=arn)
-
-    log.info("Deleting Deny policy")
-    response = iam.delete_policy(PolicyArn=arn)
 
 
 @pytest.fixture
@@ -154,7 +110,7 @@ def test_successful_execution(expected_cfg_paths, mut_output, push_changes, run_
     without any failures, expected metadb execution records were created, and expected commit status is sent.
     """
 
-    actual_cfg_paths = get_commit_cfg_paths(push_changes["commit_id"], mut_output)
+    actual_cfg_paths = get_commit_cfg_paths(push_changes["commit_id"])
 
     log.info("Assert that all expected cfg_paths are within executions table")
     assert len(expected_cfg_paths) == len(actual_cfg_paths) and sorted(
@@ -172,7 +128,7 @@ def test_successful_execution(expected_cfg_paths, mut_output, push_changes, run_
     assert status.state == "success"
 
 
-@pytest.mark.usefixtures("truncate_executions", "send_commit_status", "remove_iam_role")
+@pytest.mark.usefixtures("truncate_executions", "send_commit_status")
 @pytest.mark.parametrize(
     "push_changes",
     [
@@ -194,7 +150,7 @@ def test_failed_execution(mut_output, push_changes, run_task):
     all records associated with the commit should be rolled back and the task's
     associated commit status should set to failure.
     """
-    actual_cfg_paths = get_commit_cfg_paths(push_changes["commit_id"], mut_output)
+    actual_cfg_paths = get_commit_cfg_paths(push_changes["commit_id"])
 
     log.info(
         "Assert that are no records within the execution table from the testing commit ID"
